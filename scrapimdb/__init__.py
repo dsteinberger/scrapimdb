966b50ed690ef05ca8fdd55ecb57262b0827a00c
dsteinberger <david.steinberger@gmail.com>
2025-11-23 09:56:54 +0100

    Bump version: 0.1.7 → 0.1.8

--- scrapimdb/__init__.py
+++ scrapimdb/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """David Steinberger"""
 __email__ = "david.steinberger@gmail.com"
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 from .spider import ImdbSpider
 
--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dsteinberger/scrapimdb',
-    version='0.1.7',
+    version='0.1.8',
     zip_safe=False,
 )
